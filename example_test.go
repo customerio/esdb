8c6aaeebb81a2a061e62df399d29ca401ff82e51
John Allison <jrallison@gmail.com>
2014-06-23 20:00:54 +0300

    Add example for creating/reading an esdb.

---
+++ example_test.go
@@ -0,0 +1,111 @@
+package esdb
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type event struct {
+	customerId string
+	timestamp  int
+	eventType  string
+	data       map[string]string
+}
+
+func ExampleDb() {
+	events := []event{
+		event{"1", 1403534919, "page", map[string]string{"url": "http://mysite.com/"}},
+		event{"1", 1403534920, "click", map[string]string{"button_text": "Checkout"}},
+		event{"1", 1403534921, "page", map[string]string{"url": "http://mysite.com/checkout"}},
+		event{"1", 1403534923, "purchase", map[string]string{"total": "42.99"}},
+		event{"1", 1403534923, "page", map[string]string{"url": "http://mysite.com/thankyou"}},
+		event{"2", 1403534919, "page", map[string]string{"url": "http://mysite.com/"}},
+		event{"2", 1403534920, "click", map[string]string{"button_text": "About"}},
+		event{"2", 1403534921, "page", map[string]string{"url": "http://mysite.com/about"}},
+		event{"3", 1403534919, "page", map[string]string{"url": "http://mysite.com/"}},
+		event{"3", 1403534920, "click", map[string]string{"button_text": "About"}},
+		event{"3", 1403534921, "page", map[string]string{"url": "http://mysite.com/about"}},
+		event{"3", 1403534922, "click", map[string]string{"button_text": "Checkout"}},
+		event{"3", 1403534923, "purchase", map[string]string{"total": "126.99"}},
+		event{"3", 1403534923, "page", map[string]string{"url": "http://mysite.com/thankyou"}},
+	}
+
+	// In case we've already created the file.
+	os.Remove("activity.esdb")
+
+	writer, err := New("activity.esdb")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, e := range events {
+		data, _ := json.Marshal(e.data)
+
+		writer.Add(
+			[]byte(e.customerId), // block the event will be stored under.
+			e.timestamp,          // all events will be stored sorted by this value.
+			data,                 // value can be any binary data.
+			"",                   // grouping. "" here means no grouping, store sequentially by timestamp.
+			[]string{e.eventType}, // We'll define one secondary index on event type.
+		)
+	}
+
+	err = writer.Write()
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := Open("activity.esdb")
+	if err != nil {
+		panic(err)
+	}
+
+	// Stream through all customer 1's activity
+	fmt.Println("activity for 1:")
+	db.Find([]byte("1")).Scan("", func(event *Event) bool {
+		fmt.Println(string(event.Data))
+		return true // continue
+	})
+
+	// Stream through all customer 2's activity
+	fmt.Println("\nactivity for 2:")
+	db.Find([]byte("2")).Scan("", func(event *Event) bool {
+		fmt.Println(string(event.Data))
+		return true // continue
+	})
+
+	// Just retrieve customer 1's purchases
+	fmt.Println("\npurchases for 1:")
+	db.Find([]byte("1")).ScanIndex("purchase", func(event *Event) bool {
+		fmt.Println(string(event.Data))
+		return true // continue
+	})
+
+	// Just retrieve customer 3's clicks ordered descending
+	fmt.Println("\nclicks for 3:")
+	db.Find([]byte("3")).RevScanIndex("click", func(event *Event) bool {
+		fmt.Println(string(event.Data))
+		return true // continue
+	})
+
+	// Output:
+	// activity for 1:
+	// {"total":"42.99"}
+	// {"url":"http://mysite.com/thankyou"}
+	// {"url":"http://mysite.com/checkout"}
+	// {"button_text":"Checkout"}
+	// {"url":"http://mysite.com/"}
+	//
+	// activity for 2:
+	// {"url":"http://mysite.com/about"}
+	// {"button_text":"About"}
+	// {"url":"http://mysite.com/"}
+	//
+	// purchases for 1:
+	// {"total":"42.99"}
+	//
+	// clicks for 3:
+	// {"button_text":"Checkout"}
+	// {"button_text":"About"}
+}
